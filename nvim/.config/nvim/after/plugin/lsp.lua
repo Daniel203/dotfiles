d628ac180c9d6f959edc3eb923a2883eb85fc68c
Daniel Arduini <danielarduini47@gmail.com>
2022-12-25 11:34:41 +0100

    nvim format-on-save

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,20 +2,43 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
+lsp.on_attach(function(_, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>fl", "<cmd>LspZeroFormat<CR>")
+
+    -- format on save
+    vim.cmd [[augroup Format]]
+    vim.cmd [[autocmd! * <buffer>]]
+    vim.cmd [[autocmd BufWritePre *.* LspZeroFormat]]
+    vim.cmd [[augroup END]]
+end)
+
+
 lsp.ensure_installed({
-  'efm',
+    'efm',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
 
 lsp.setup()
 
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = true,
-  float = true,
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = true,
 })
-
--- Format on save
-vim.cmd [[autocmd BufWritePre *.* LspZeroFormat]]
